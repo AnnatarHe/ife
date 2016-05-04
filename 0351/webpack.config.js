949ca3fe53e67b383bbd7b2b1721c591940ae52a
AnnatarHe <hele@iamhele.com>
2016-05-04 20:40:16 +0800

    init task 03 51

---
+++ 0351/webpack.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+module.exports = {
+    entry: {
+      index: path.resolve('./lib/app.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist',
+        filename: 'app.js'
+    },
+    devtool: 'eval-source-map',
+    module: {
+        loaders: [{
+            test: /.js$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }, {
+            test: /.sass$/,
+            exclude: /node_modules/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'sass-loader')
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css', {
+            disable: false,
+            allChunks: true
+        })
+   ]
+}
