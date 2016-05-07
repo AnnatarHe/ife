9bba2278c317af11c463f41b15a9746de762e387
AnnatarHe <hele@iamhele.com>
2016-05-07 23:57:32 +0800

    add comments at ultils functions

--- 0351/lib/utils/index.js
+++ 0351/lib/utils/index.js
@@ -1,3 +1,10 @@
+/**
+ * 使用higher来覆盖默认配置用的
+ * 没有进行深度遍历
+ * @param  {Object} base   默认配置
+ * @param  {Object} higher 用户配置
+ * @return {Object}        没有修改base，而是创建一个中间变量来保持纯度
+ */
 const extend = (base, higher) => {
     if (base === undefined || higher === undefined) {
         return false
@@ -13,6 +20,10 @@ const extend = (base, higher) => {
     return _temp
 }
 
+/**
+ * 是否支持全屏
+ * @return {Boolean} 
+ */
 const isFullscreenEnabled = () => {
     // 懒得管兼容性了
     return document.fullscreenEnabled
