f8b1e4540087bfaa0f7b29ea2c1c40b1d2196b1e
AnnatarHe <hele@iamhele.com>
2016-05-19 23:29:37 +0800

    add barrel module

--- 0351/lib/barrel/barrel.js
+++ 0351/lib/barrel/barrel.js
@@ -3,6 +3,29 @@ import './barrel.sass'
 import Album from '../albumInterfaces/album'
 
 class Barrel extends Album {
+    
+    constructor(imageDoms, options) {
+        super()
+
+        this.options = {
+
+            barrelHeight: 16 || options.barrelHeight, // 木桶模式的高度
+            barrelBinMin: 5 || options.barrelBinMin, // 木桶模式每行图片数量下限
+            barrelBinMax: 10 || options.barrelBinMax
+        }
+    }
+    
+    init(imageDoms) {
+        throw new Error('please implement init method')
+    }
+
+    append(image) {
+        throw new Error('please implement append method')
+    }
+
+    render() {
+        throw new Error('please implement render method')
+    }
 
 }
 
